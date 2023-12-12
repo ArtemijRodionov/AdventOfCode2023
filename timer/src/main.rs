a35a30c1f21be418458e6b715a9d6c81f2f72fde
Artemiy Rodionov <wertins@yandex-team.ru>
2023-12-12 15:18:49 +0300

    total

--- timer/src/main.rs
+++ timer/src/main.rs
@@ -1,4 +1,4 @@
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
 fn puzzles() -> &'static [(fn(), &'static str)] {
     &[
@@ -6,17 +6,21 @@ fn puzzles() -> &'static [(fn(), &'static str)] {
         (day02::main, "2"),
         (day03::main, "3"),
         (day04::main, "4"),
-        (day05::main, "5"),
+        // (day05::main, "5"),
         (day06::main, "6"),
         (day07::main, "7"),
     ]
 }
 
 pub fn main() {
+    let mut total = Duration::new(0, 0);
+
     for (f, name) in puzzles() {
         let now = Instant::now();
         f();
         let elapsed = now.elapsed();
+        total += elapsed;
         println!("{} took {:.3}ms", name, elapsed.as_secs_f64() * 1000.0)
     }
+    println!("Total {:.3}ms", total.as_secs_f64() * 1000.0)
 }
